e23d521d3f1b3d6d618802fc3bf4789e75c94ce3
valenti-casas-ilerna <vcasas@ilerna.com>
2020-10-09 16:32:42 +0200

    Creació de components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { SignComponent } from './components/sign/sign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    HeaderComponent,
+    HomeComponent,
+    GalleryComponent,
+    AboutUsComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,
